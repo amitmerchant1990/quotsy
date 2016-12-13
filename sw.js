f6153fa3d204ce19f7d21937e85c76c04d64f334
Amit Merchant <bullredeyes@gmail.com>
2016-12-13 23:58:42 +0530

    init

---
+++ sw.js
@@ -0,0 +1,110 @@
+importScripts('js/cache-polyfill.js');
+
+var CACHE_VERSION = 'app-v20';
+var CACHE_FILES = [
+    '/',
+    'index.html',
+    'js/app.js',
+    'js/jquery.min.js',
+    'js/bootstrap.min.js',
+    'js/pulltorefresh.js',
+    'css/bootstrap.min.css',
+    'css/style.css',
+    'favicon.ico',
+    'manifest.json',
+    'img/icon-48.png',
+    'img/icon-96.png',
+    'img/icon-144.png',
+    'img/icon-196.png'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_VERSION)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(CACHE_FILES);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function(res){
+            if(res){
+                return res;
+            }
+            requestBackend(event);
+        })
+    )
+});
+
+function requestBackend(event){
+    var url = event.request.clone();
+    return fetch(url).then(function(res){
+        //if not a valid response send the error
+        if(!res || res.status !== 200 || res.type !== 'basic'){
+            return res;
+        }
+
+        var response = res.clone();
+
+        caches.open(CACHE_VERSION).then(function(cache){
+            cache.put(event.request, response);
+        });
+
+        return res;
+    })
+}
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function(keys){
+            return Promise.all(keys.map(function(key, i){
+                if(key !== CACHE_VERSION){
+                    return caches.delete(keys[i]);
+                }
+            }))
+        })
+    )
+});
+
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
+
+  var title = 'Yay a message.';
+  var body = 'We have received a push message.';
+  var icon = '/images/icon-192x192.png';
+  var tag = 'simple-push-demo-notification-tag';
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn’t close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: 'window'
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url === '/' && 'focus' in client) {
+        return client.focus();
+      }
+    }
+    if (clients.openWindow) {
+      return clients.openWindow('/');
+    }
+  }));
+});
